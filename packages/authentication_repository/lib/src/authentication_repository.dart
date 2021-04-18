db83ba7cf54dea74b9db777851784063aedeae2d
mark-taeyoung <mark.taeyoung@icloud.com>
2021-04-18 15:15:53 +0900

    cache and model

--- packages/authentication_repository/lib/src/authentication_repository.dart
+++ packages/authentication_repository/lib/src/authentication_repository.dart
@@ -1,9 +1,10 @@
 import 'dart:async';
 
 import 'package:authentication_repository/authentication_repository.dart';
+import 'package:cache/cache.dart';
 import 'package:firebase_auth/firebase_auth.dart' as firebase_auth;
 import 'package:google_sign_in/google_sign_in.dart';
-// import 'package:meta/meta.dart';
+import 'package:meta/meta.dart';
 
 /// Thrown if during the sign up process if a failure occurs.
 class SignUpFailure implements Exception {}
@@ -23,31 +24,44 @@ class LogOutFailure implements Exception {}
 class AuthenticationRepository {
   /// {@macro authentication_repository}
   AuthenticationRepository({
+    CacheClient? cache,
     firebase_auth.FirebaseAuth? firebaseAuth,
     GoogleSignIn? googleSignIn,
-  })  : _firebaseAuth = firebaseAuth ?? firebase_auth.FirebaseAuth.instance,
+  })  : _cache = cache ?? CacheClient(),
+        _firebaseAuth = firebaseAuth ?? firebase_auth.FirebaseAuth.instance,
         _googleSignIn = googleSignIn ?? GoogleSignIn.standard();
 
+  final CacheClient _cache;
   final firebase_auth.FirebaseAuth _firebaseAuth;
   final GoogleSignIn _googleSignIn;
+
+  /// User cache key.
+  /// Should only be used for testing purposes.
+  @visibleForTesting
+  static const userCacheKey = '__user_cache_key__';
 
   /// Stream of [User] which will emit the current user when
   /// the authentication state changes.
   ///
-  /// Emits [User.empty] if the user is not authenticated.
+  /// Emits [User.anonymous] if the user is not authenticated.
   Stream<User> get user {
     return _firebaseAuth.authStateChanges().map((firebaseUser) {
-      return firebaseUser == null ? User.anonymous : firebaseUser.toUser;
+      final user = firebaseUser == null ? User.anonymous : firebaseUser.toUser;
+      _cache.write(key: userCacheKey, value: user);
+      return user;
     });
+  }
+
+  /// Returns the current cached user.
+  /// Defaults to [User.anonymous] if there is no cached user.
+  User get currentUser {
+    return _cache.read<User>(key: userCacheKey) ?? User.anonymous;
   }
 
   /// Creates a new user with the provided [email] and [password].
   ///
   /// Throws a [SignUpFailure] if an exception occurs.
-  Future<void> signUp({
-    required String email,
-    required String password,
-  }) async {
+  Future<void> signUp({required String email, required String password}) async {
     try {
       await _firebaseAuth.createUserWithEmailAndPassword(
         email: email,
@@ -60,7 +74,7 @@ class AuthenticationRepository {
 
   /// Starts the Sign In with Google Flow.
   ///
-  /// Throws a [LogInWithGoogleFailure] if an exception occurs.
+  /// Throws a [logInWithGoogle] if an exception occurs.
   Future<void> logInWithGoogle() async {
     try {
       final googleUser = await _googleSignIn.signIn();
@@ -93,7 +107,7 @@ class AuthenticationRepository {
   }
 
   /// Signs out the current user which will emit
-  /// [User.empty] from the [user] Stream.
+  /// [User.anonymous] from the [user] Stream.
   ///
   /// Throws a [LogOutFailure] if an exception occurs.
   Future<void> logOut() async {
